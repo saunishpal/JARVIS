678de5f4f26552664e651d049aceee8a19587fc3
saunishpal <saunishpal@gmail.com>
2025-07-03 10:18:31 +0530

    Update client.py

--- main jarvis 2.0/client.py
+++ main jarvis 2.0/client.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 client = OpenAI(
-    api_key="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"
+    api_key=
 )
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
